14b28c59d5b942f4b409f730e2d63bd0566a0484
caru1613 <caru1613@gmail.com>
2021-05-01 08:17:31 +0000

    LAB_4 assignment
    Signed-off-by: caru1613 <caru1613@gmail.com>

--- lab_assignment/lab_4/linux_mac/fahrenheit_converter.py
+++ lab_assignment/lab_4/linux_mac/fahrenheit_converter.py
@@ -3,10 +3,22 @@ def convert_celsius_fahrenheit(a):
     fahrenheit_value = float(((9/5)*a) + 32)
     return fahrenheit_value
 
+def input_celsius_value():
+    print("변환하고 싶은 섭씨 온드를 입력해 주세요:")
+    celsius_value = float(input())
+    return celsius_value
+
+def print_fahrenheit_value(celsius_value, fahrenheit_value):
+    print("섭씨온도 :" +  str(celsius_value))
+    print("화씨온도 :" +  str(fahrenheit_value))
+
 def main():
     print("본 프로그램은 섭씨를 화씨로로 변환해주는 프로그램입니다")
     print("============================")
     # ===Modify codes below=================
+    celsius_value = input_celsius_value()
+    fahrenheit_value = convert_celsius_fahrenheit(celsius_value)
+    print_fahrenheit_value(celsius_value, fahrenheit_value)
 
     # ======================================
     print("===========================")

3ab42ecfef32fb81bb3d8755c70544ad12ee6038
caru1613 <caru1613@gmail.com>
2021-11-12 06:52:41 +0000

    LAB Logger
    Signed-off-by: caru1613 <caru1613@gmail.com>

---
+++ lab_assignment/lab_logging/logging.py
@@ -0,0 +1,13 @@
+import logging
+
+logger = logging.getLogger("main")
+stream_handler = logging.StreamHandler()
+logger.addHandler(stream_handler)
+
+
+logger.debug("Debug")
+logger.info("Info")
+logger.warning("Warning")
+logger.error("Error")
+logger.critical("Critical")
+

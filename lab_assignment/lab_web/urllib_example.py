3c74e5a50dce4a4ae8c0a106ec585a665eac68e6
caru1613 <caru1613@gmail.com>
2022-02-06 01:14:02 +0900

    web
    Signed-off-by: caru1613 <caru1613@gmail.com>

---
+++ lab_assignment/lab_web/urllib_example.py
@@ -0,0 +1,7 @@
+import urllib.request
+
+url = "http://storage.googleapis.com/patents/grant_full_text/2014/ipg140107.zip"
+
+print("Start Dwonload")
+fname, header = urllib.request.urlretrieve(url, 'ipg140107.zip')
+print("End Download")
